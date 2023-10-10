5357b23ba21429c45f55460ee580a2b4eae67765
Anya <use.anya@gmail.com>
2023-10-10 22:58:04 +0300

    add user model and post request to register a new user

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import {registerValidation} from './validations/auth.js';
+import {validationResult} from "express-validator";
+import UserModel from './models/User.js';
 
 mongoose.connect("mongodb+srv://leegra:jAcvtnnWF2qmBTdK@cluster0.o97bigy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp").then(()=>{
     console.log("DB ok");
@@ -16,17 +19,26 @@ app.get('/', (req, res) => {
     res.send("Hey");
 });
 
-app.post('/auth/login', (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
+    const errors=validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json(errors.array());
+    }
 
-    const token = jwt.sign({
-        login: req.body.login,
-        password: req.body.password
-    }, 'secret');
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash=await bcrypt.hash(password, salt);
 
-    res.json({
-        success: true,
-        token: token
+    const data = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash
     });
+
+    const user = await data.save();
+
+    res.json(user);
 });
 
 app.listen(4444, (err)=>{
@@ -34,4 +46,4 @@ app.listen(4444, (err)=>{
         return console.log(err);
     }
     console.log("server ok");
-})
+});
